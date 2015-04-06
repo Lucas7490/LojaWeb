957a6ada2f90d748f0bfc6d5ca8a35517b31f5c7
31358535 <31358535@099085-P31S202.macklabs.br>
2015-04-06 19:56:10 -0300

    DAO e algumas entidades

---
+++ Mackenzie/4º semestre/LP3/projeto - semana 06.04.2015/LojaWebLP3/src/br/mack/dao/ProdutoDAO.java
@@ -0,0 +1,32 @@
+package br.mack.dao;
+
+import br.mack.modeloEntidade.Produto;
+import java.util.Set;
+
+/**
+ *
+ * @author 31358535
+ */
+public class ProdutoDAO extends DAO<Object>{
+
+    @Override
+    public void create(Object el) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Produto read(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Produto delete(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public Set<Produto> readAll() {return null;}
+}
